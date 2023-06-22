f8562916aec3741ed62aa85448805d35107196a0
AnthonyLopezz <anthony.lopez@usantoto.edu.co>
2023-06-22 18:12:44 -0500

    origin main

--- config/config.js
+++ config/config.js
@@ -10,21 +10,21 @@ types.setTypeParser(1114, function (stringValue) {
   return stringValue;
 });
 
-// const databaseConfig = {
-//     'host': 'localhost',
-//     'port': 5432,
-//     'database': 'esmarthealth',
-//     'user': 'esmarthealth',
-//     'password': 'eshp.2022'
-// };
-
 const databaseConfig = {
-  host: "127.0.0.1",
-  port: 5432,
-  database: "smarthealth_db",
-  user: "postgres",
-  password: "DataPostGF104",
+    'host': 'localhost',
+    'port': 5432,
+    'database': 'esmarthealth',
+    'user': 'esmarthealth',
+    'password': 'eshp.2022'
 };
+
+// const databaseConfig = {
+//   host: "127.0.0.1",
+//   port: 5432,
+//   database: "smarthealth_db",
+//   user: "postgres",
+//   password: "DataPostGF104",
+// };
 
 const db = pgp(databaseConfig);
 
